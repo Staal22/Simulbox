219f72deba28673dbe215cfd5808d6bd2aab25d9
Staal22 <staalbr@gmail.com>
2023-11-04 23:34:32 +0100

    uhhhhhhhhhhh

--- Simulation/Simulbox/Assets/Code/Managers/InputManager.cs
+++ Simulation/Simulbox/Assets/Code/Managers/InputManager.cs
@@ -7,24 +7,33 @@ using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject outlineIndicatorPrefab;
+    
     private CommandInvoker _commandInvoker;
 
     private void Start()
     {
         _commandInvoker = new CommandInvoker();
+        outlineIndicatorPrefab = Instantiate(outlineIndicatorPrefab);
     }
 
     private void Update()
     {
+        // if we're not hovering over a UI element
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            outlineIndicatorPrefab.SetActive(false);
+            return;
+        }
+        outlineIndicatorPrefab.SetActive(true);
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            outlineIndicatorPrefab.transform.position = hit.point;
+        }
         if (Input.GetMouseButtonDown(0))
         {
-            // if we didn't hit UI element
-            if (EventSystem.current.IsPointerOverGameObject())
-            {
-                return;
-            }
-            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            if (Physics.Raycast(ray, out var hitInfo))
             {
                 // ICommand command = new AddVoxelCommand(hitInfo.point);
                 ICommand command = new AddVoxelGroup(hitInfo.point);
